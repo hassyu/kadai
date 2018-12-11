81f986d638ac159e8282adbb413db57c6dbcff5e
hassyu <45799304+hassyu@users.noreply.github.com>
2018-12-12 03:35:52 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<pre>
+<?php
+	include ('html/header.html');
+	include ('html/list.html');
+	$fname="count.txt";
+	$fp=fopen($fname,"r+");
+	fscanf($fp,"%d",$n);
+	if(date('Ymd')!=date('Ymd',filemtime($fname))){
+		$n=0;
+	}
+	$n++;
+	echo "あなたは、本日";
+	for($i = 0; $i < strlen($n); $i++){
+		$x=substr($n,$i,1);
+		echo "<img src='sozai/$x.png'>";
+	}
+	echo "人目の訪問者です。";	
+	rewind($fp);
+	fprintf($fp,"%d",$n);
+	fclose($fp);
+	$today=date('y/m/d h:i:s');
+	echo $today;
+?>
